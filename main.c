24beffe0bc8b400dcd52a9b2eedbde8e37c03227
AlgoRythm-Dylan <dperunko@comcast.net>
2017-09-28 20:06:53 -0400

    Getting a little code out there. Just an almost empty main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <ncurses.h>
+#include <lua.h>
+
+/*
+
+    I'm sorry in advance about the code quality here -
+    this is a learning project for me.
+
+    lncurses: Lua ncurses bindings by AlgoRythm
+    (https://github.com/AlgoRythm-Dylan)
+
+    License: MIT
+
+*/
+
+#define VERSION "0.1.0"
+
+int main()
+{
+    return 0;
+}
